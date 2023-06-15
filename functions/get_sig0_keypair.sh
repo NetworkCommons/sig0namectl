f47bf5e0ba9a40146f92d0834c1949be70bf432c
vortex <vortex@free2air.net>
2023-06-15 21:48:59 +0200

    adds get_sig0_keypair function and minor improvements

---
+++ functions/get_sig0_keypair.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+
+
+SIG0_KEYPATH=${SIG0_KEYPATH:-"${PWD}"}
+
+get_sig0_keypair() {
+	# get_sig0_keypair( refvar, fqdn, search_path )
+	#
+	# 	refvar: global env var passed by reference will be set to *.[key|private] keypair filename prefix if found, or "" if not.
+	#
+	# 	fqdn: FQDN portion of keypair filenames to search and return, eg host.exmple.com
+	#
+	# 	search_path: path to search for *.key and *.private files
+	#
+	declare -n refvar="${1}"
+	local fqdn="${2}"
+	local search_path="${3:-${SIG0_KEYPATH}}"
+
+	# get list of matching files
+	#
+	local FIND=$(find ${search_path:-${SIG0_KEYPATH}} -type f \( -iname K${fqdn}.\*.key -o -iname K${fqdn}.\*.private \))
+	# split list into array
+	local ar=(${FIND})
+	local PREFIX=""
+	[[ -n ${TEST} ]] && printf "==================== calling ${FUNCNAME[0]} ${1} ${2} ${3}\n"
+	
+	if (( ${#ar[@]} < 2 )); then
+		refvar=""
+		[[ -n ${DEBUG} ]] && printf "Warning: ${FUNCNAME[0]}(): path var set to \"${refvar}\": no keypair files for ${fqdn} found under ${search_path}.\n"
+		return 1
+	fi
+	
+	if (( ${#ar[@]} > 2 )); then
+		refvar=""
+		[[ -n ${DEBUG} ]] && printf "Warning: ${FUNCNAME[0]}(): path var set to \"${refvar}\": multiple keypair files for ${fqdn} found under ${search_path}.\n"
+		return 1
+	fi
+	
+	if [ "${ar[0]%.*}" != "${ar[1]%.*}" ]; then
+		refvar=""
+		[[ -n ${DEBUG} ]] && printf "Warning: ${FUNCNAME[0]}(): path var set to \"${refvar}\": no matching keypair files for ${fqdn} found under ${search_path}.\n"
+		return 1
+	fi
+
+	PREFIX="${ar[0]%.*}"
+	refvar="${PREFIX##*/}"
+	[[ -n ${DEBUG} ]] && printf "Info: ${FUNCNAME[0]}(): set to \"${refvar}\": Unique keypair prefix found: ${PREFIX} in ${search_path}\n"
+
+}
+
+
+if [[ -n ${TEST} ]]; then
+	printf "** TEST get_sig0_keypair()\n"
+	DEBUG="true"
+	get_sig0_keypair SIG0_KEYPAIR zembla.zenr.io ${SIG0_KEYPATH}
+	echo "SIG0_KEYPAIR set to '${SIG0_KEYPAIR}'"
+	get_sig0_keypair SIG0_KEYPAIR vortex.zenr.io
+	echo "SIG0_KEYPAIR set to '${SIG0_KEYPAIR}'"
+	get_sig0_keypair SIG0_KEYPAIR no.such.name ${SIG0_KEYPATH}
+	echo "SIG0_KEYPAIR set to '${SIG0_KEYPAIR}'"
+	exit
+fi
