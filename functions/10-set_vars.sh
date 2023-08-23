77a3c48db94e7a42778a98e4d713450b4210288b
vortex <adamb@free2air.net>
2023-08-23 14:45:25 +0200

    adds getopts() handling, set_env now requires  arg

--- functions/10-set_vars.sh
+++ functions/10-set_vars.sh
@@ -4,44 +4,76 @@ set_vars() {
 	# set_vars
 	#
 	# sets environment variables from env files
-	#
+	# set_vars $*
+	# 	function also handles invocation paramters & requires parameters passed
 	SCRIPT_NAME="${0#*/}"
 
 	# Source env file for project-wide variable default values
 	ENV_FILE=${ENV_FILE:-"env"}
-	if [ -e ${ENV_FILE} ]
-	then
-	        . ./${ENV_FILE}
-	        [[ -n ${DEBUG_SOURCED} ]] && echo "Sourced ${PWD}/${ENV_FILE} ..."
+	if [ -e ${ENV_FILE} ]; then
+		. ./${ENV_FILE}
+		[[ -n ${DEBUG_SET_VARS} ]] && echo "Sourced ${PWD}/${ENV_FILE} ..."
 	fi
 
 	# Source env file for script-wide default values
-	if [ -e ${ENV_FILE}.${SCRIPT_NAME} ]
-	then
-	        . ${ENV_FILE}.${SCRIPT_NAME}
-	        [[ -n ${DEBUG_SOURCED} ]] && echo "Sourced ${PWD}/${ENV_FILE}.${SCRIPT_NAME} ..."
+	if [ -e ${ENV_FILE}.${SCRIPT_NAME} ]; then
+		source ${ENV_FILE}.${SCRIPT_NAME}
+		[[ -n ${DEBUG_SET_VARS} ]] && echo "Sourced ${PWD}/${ENV_FILE}.${SCRIPT_NAME} ..."
+	fi
+	
+	while getopts ":dk:s:" ARG;
+	do
+		case "${ARG}" in
+			d)
+				NSUPDATE_ACTION="delete"
+				[[ -n ${DEBUG_SET_VARS} ]] && echo "-d passed: delete action set"
+				;;
+			s)
+				NSUPDATE_SIG0_KEYPATH="${OPTARG}"
+				[[ -n ${DEBUG_SET_VARS} ]] && echo "-s passed: explicit sig0 keystore path parameter '${OPTARG}' given"
+				;;
+			k)
+				NSUPDATE_AUTH_SIG0_KEY_FQDN="${OPTARG}"
+				[[ -n ${DEBUG_SET_VARS} ]] && echo "-k passed: explicit sig0 key fqdn parameter '${OPTARG}' given"
+				;;
+		esac
+	done
+	shift "$((OPTIND-1))"
+
+	NEW_FQDN="${NEW_FQDN:-${1}}"
+	if [[ ! -n ${NEW_FQDN} ]]; then
+		if [[ -n ${ZONE} ]]; then
+			if [[ -n ${NEW_SUBZONE} ]]; then
+				NEW_FQDN="${NEW_SUBZONE}.${ZONE}"
+			else
+				NEW_FQDN="${ZONE}"
+			fi
+		else
+			echo "No ZONE var set or FQDN argument given" && exit 1
+		fi
 	fi
 
-	# Default existing ZONE fallback to $DOMAINNAME if set, else to domain set in $HOSTNAME, else error
-	# ZONE MUST correspond to an existing DNS ZONE, that is resolve with an SOA record
-	ZONE=${ZONE:-${DOMAINNAME}}
-	ZONE=${ZONE:-${HOSTNAME#*.}}
-	if [[ ! -n ${ZONE} ]]; then
-	        echo "Error: DNS zone \$ZONE environment variable is not set & could not be determined from \$DOMAINNAME or \$HOSTNAME"
-	        exit 1
-	fi
-
-	# path to search for nsupdate format key pairs
+	# Last attempt to set default sigo keystore path
 	NSUPDATE_SIG0_KEYPATH=${NSUPDATE_SIG0_KEYPATH:-"${PWD}"}
 
-	DIG_QUERY_PARAM=${DIG_QUERY_PARAM:-}
-	AVAHI_BROWSE_PARAM=${AVAHI_BROWSE_PARAM:-"-brat"}
-	
+	ZONE="${ZONE:-$(get_soa ${NEW_FQDN})}" # sanity check?
+	NEW_SUBZONE=${NEW_SUBZONE:-${NEW_FQDN%*.${ZONE}}}
+	[[ -n ${DEBUG} ]] && echo "ZONE='${ZONE}'" && echo "NEW_SUBZONE='${NEW_SUBZONE}'" && echo "NEW_FQDN='${NEW_FQDN}'"
+	if [[ ${#ZONE} < 2 ]]; then
+		echo "Error: DNS zone ZONE='${ZONE}' environment variable is not set & could not be determined from \$DOMAINNAME or \$HOSTNAME"
+		echo "DEBUG: NEW_FQDN='${NEW_FQDN}'"
+		exit 1
+	fi
+
 	# Discover master (usually primary DNS server) of zone from master field of SOA record
 	#
 	ZONE_SOA_MASTER=$( get_soa_master ${ZONE} )
 	if [[ ! -n ${ZONE_SOA_MASTER} ]]; then
-	        echo "Warning: ZONE ${ZONE} SOA record does not resolve"
-	        exit 1
+		echo "Error: Could not resolve SOA record for ZONE '${ZONE}'"
+		exit 1
 	fi
+
+	# External tool defaults
+	DIG_QUERY_PARAM=${DIG_QUERY_PARAM:-}
+	AVAHI_BROWSE_PARAM=${AVAHI_BROWSE_PARAM:-"-brat"}
 }
