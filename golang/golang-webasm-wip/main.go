2e9cf29185eb787d8d203a6d13c6d79b131dc35c
vortex <adamb@free2air.net>
2024-04-15 15:16:49 +0200

    restores complete chain

--- golang/golang-webasm-wip/main.go
+++ golang/golang-webasm-wip/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-	"os"
+	// "os"
 
 	"github.com/miekg/dns"
+	"github.com/shynome/doh-client"
 )
 
 func main() {
-	name := os.Args[1]
+	// name := os.Args[1]
+	name := "cryptix.zenr.io"
+	server := "zembla.zenr.io"
 	q := dns.Question{
 		Name:   dns.Fqdn(name),
 		Qtype:  dns.TypeA,
@@ -23,28 +26,35 @@ func main() {
 	out, err := m.Pack()
 	check(err)
 
-	fmt.Println("Q:", base64.StdEncoding.EncodeToString(out))
+	// fmt.Printf("Q:(TXT): %s\t\t\t%d\n", q.Name, q.Qtype)
+	fmt.Printf("Q:(TXT):%v\n", q)
+	fmt.Println("Q:(b64):", base64.StdEncoding.EncodeToString(out))
+	fmt.Println("Q:(DoH):", "https://"+server+"/dns-query="+base64.StdEncoding.EncodeToString(out))
 
-	inputStr := "9beFAAABAAIAAAAAB2NyeXB0aXgEemVucgJpbwAAAQABwAwAAQABAAAAPAAEX9ko/cAMAAEAAQAAADwABFSiXZs="
+	inputStr := "lzeFAAABAAIAAAAAB2NyeXB0aXgEemVucgJpbwAAAQABwAwAAQABAAAAPAAEX9ko/cAMAAEAAQAAADwABFSiXZs="
 	input, err := base64.StdEncoding.DecodeString(inputStr)
 	check(err)
 	var resp = new(dns.Msg)
 
 	err = resp.Unpack(input)
 	check(err)
-	fmt.Printf("A: +%v\n", resp.Answer)
+	// fmt.Printf("A: +%v\n", resp.Answer)
 
-	// co := &dns.Conn{Conn: doh.NewConn(nil, nil, "1.1.1.1")}
-
-	// err := co.WriteMsg(m)
+	co := &dns.Conn{Conn: doh.NewConn(nil, nil, server)}
+	if err := co.WriteMsg(m); err != nil {
+		panic(err)
+	}
 	// check(err)
 
-	// m, err = co.ReadMsg()
-	// check(err)
-	// if len(m.Answer) == 0 {
-	// 	panic("answer length must greater than 0")
-	// }
-	// fmt.Printf("Answer: %+v\n", m.Answer)
+	m, err = co.ReadMsg()
+	check(err)
+	if len(m.Answer) == 0 {
+		panic("answer length must greater than 0")
+	}
+	// fmt.Printf("A:(TXT):%+v\n", m.Answer)
+	for _, str := range m.Answer {
+		fmt.Printf("A:(TXT): %s\n",str)
+	}
 }
 
 func check(err error) {
