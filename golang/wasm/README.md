bfa3b25483bd424228d0fd083413ae530c81d52b
Henry <cryptix@riseup.net>
2024-04-16 19:55:48 +0200

    better calling trickery

--- golang/wasm/README.md
+++ golang/wasm/README.md
@@ -1,4 +1,22 @@
-Links:
+# WebAssembly DNS Demo
+
+## Function access
+
+```js
+const {query, parse} = window.goFuncs
+
+const q = query("test.somewhe.re")
+
+const resp = await fetch(`https://zembla.zenr.io/dns-query?dns=${q}`)
+// TODO: assert status == 200
+const data = await resp.arrayBuffer()
+
+const answer = parse(_arrayBufferToBase64(data))
+console.log(answer)
+```
+
+
+## Links
 
 * https://golangbot.com/webassembly-using-go/
 
--- golang/wasm/q_js.go
+++ golang/wasm/q_js.go
@@ -15,24 +15,39 @@ import (
 */
 
 func main() {
-	var query, parse js.Func
-	query = js.FuncOf(func(this js.Value, args []js.Value) any {
+	var query, queryFromDOM, parse js.Func
+	queryFromDOM = js.FuncOf(func(this js.Value, args []js.Value) any {
 		domainName := js.Global().Get("document").Call("getElementById", "domain-name").Get("value")
+
 		fmt.Println("Domain:", domainName)
-		q := sig0.QueryA(domainName.String())
+		q := sig0.QueryAny(domainName.String())
 		js.Global().Get("document").Call("getElementById", "query-data").Set("value", q)
-		return nil
+		return q
 	})
-	js.Global().Get("document").Call("getElementById", "prepare").Call("addEventListener", "click", query)
+	js.Global().Get("document").Call("getElementById", "prepare").Call("addEventListener", "click", queryFromDOM)
 
 	parse = js.FuncOf(func(this js.Value, args []js.Value) any {
 		answer := js.Global().Get("document").Call("getElementById", "dns-answer").Get("value")
 		msg, err := sig0.ParseBase64Answer(answer.String())
 		check(err)
 		js.Global().Get("document").Call("getElementById", "pretty").Set("innerHTML", spew.Sdump(msg))
-		return nil
+		return msg
 	})
 	js.Global().Get("document").Call("getElementById", "parse-answer").Call("addEventListener", "click", parse)
+
+	query = js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) != 1 {
+			panic("expected 1 argument")
+		}
+		domainName := args[0].String()
+		fmt.Println("Domain:", domainName)
+		q := sig0.QueryAny(domainName)
+		return q
+	})
+
+	// setup functions for access from js side
+	js.Global().Get("window").Get("goFuncs").Set("query", query)
+	js.Global().Get("window").Get("goFuncs").Set("parse", parse)
 
 	// cant let main return
 	forever := make(chan bool)
