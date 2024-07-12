fadb60769cedaf6d787d714cb1405beacb04f91f
vortex <adamb@free2air.net>
2024-07-12 16:40:16 +0200

    adds listKeysByRR

--- golang/wasm/wrapper_js.go
+++ golang/wasm/wrapper_js.go
@@ -19,8 +19,10 @@ func main() {
 	goFuncs := js.Global().Get("window").Get("goFuncs")
 
 	goFuncs.Set("listKeys", js.FuncOf(listKeys))
+	goFuncs.Set("listKeysByRR", js.FuncOf(listKeysByRR))
 	goFuncs.Set("newKeyRequest", js.FuncOf(newKeyRequest))
 	goFuncs.Set("newUpdater", js.FuncOf(newUpdater))
+	goFuncs.Set("getSoa", js.FuncOf(getSoa))
 
 	// cant let main return
 	forever := make(chan bool)
@@ -43,7 +45,70 @@ func listKeys(_ js.Value, _ []js.Value) any {
 	for i, k := range keys {
 		values[i] = k
 	}
+	spew.Dump(keys)
 	return values
+}
+func listKeysByRR(_ js.Value, _ []js.Value) any {
+	keys, err := sig0.ListKeysByRR(".")
+	if err != nil {
+		panic(err)
+	}
+	var values = make([]any, len(keys))
+	for i, k := range keys {
+		values[i] = k
+	}
+	spew.Dump(keys)
+	return values
+}
+//
+func getSoa(_ js.Value, args []js.Value) any {
+	if len(args) != 2 {
+		return "expected 2 argument: domainName and dohServer"
+	}
+	domainName := args[0].String()
+	dohServer := args[1].String()
+
+  log.Println("getSoa: Creating SOA query msg structure for ", domainName, "to send to ", dohServer)
+
+	qry, err := sig0.QuerySOA(domainName)
+	if err != nil {
+		return err.Error()
+	}
+  fmt.Println("getSoa: DNS Query:")
+  spew.Dump(qry)
+
+	handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		resolve := args[0]
+		reject := args[1]
+
+		go func() {
+      log.Println("getSoa: handler: Requesting SOA via sig0.SendDOHQuery for", domainName, "from", dohServer)
+
+			var answer *dns.Msg
+			answer, err = sig0.SendDOHQuery(dohServer, qry)
+			if err != nil {
+				err = fmt.Errorf("Failed to create SOA query message: %w", err)
+				reject.Invoke(jsErr(err))
+				return
+			}
+			if answer.Rcode != dns.RcodeSuccess {
+				err =fmt.Errorf("unsuccessful SOA query answer\n:%#v", answer)
+				reject.Invoke(jsErr(err))
+				return
+			}
+
+      log.Println("getSoa: handler: Received SOA response via sig0.SendDOHQuery for", domainName, "from", dohServer)
+      fmt.Println("getSoa: handler: DNS Response:")
+      spew.Dump(answer)
+
+      resolve.Invoke(js.Null())
+		}()
+
+      return nil
+	})
+
+  promiseConstructor := js.Global().Get("Promise")
+	return promiseConstructor.New(handler)
 }
 
 // create a keypair and request a key
