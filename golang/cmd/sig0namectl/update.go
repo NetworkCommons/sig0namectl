706804af09f0d191de137020ba65878726d59863
Henry <cryptix@riseup.net>
2024-12-06 12:39:38 +0100

    add generic update RR via stdin

--- golang/cmd/sig0namectl/update.go
+++ golang/cmd/sig0namectl/update.go
@@ -3,9 +3,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -28,6 +30,15 @@ var updateCmd = &cli.Command{
 			Flags: []cli.Flag{
 				&cli.BoolFlag{Name: "unset", Value: true, Usage: "unset the A record, before adding one"},
 				&cli.BoolFlag{Name: "remove", Usage: "remove the A record"},
+			},
+		},
+
+		{
+			Name:   "rr",
+			Usage:  "update rr",
+			Action: updateRRAction,
+			Flags: []cli.Flag{
+				&cli.BoolFlag{Name: "remove", Usage: "remove RRs"},
 			},
 		},
 	},
@@ -81,7 +92,7 @@ func updateAAction(cCtx *cli.Context) error {
 		return err
 	}
 	zone := soaReply.Hdr.Name
-	fmt.Println("SOA lookup for", host, "found zone:", zone)
+	log.Println("-- SOA lookup for", host, "found zone:", zone)
 
 	dohServer, err := sig0.FindDOHEndpoint(zone)
 	if err != nil {
@@ -136,6 +147,108 @@ func updateAAction(cCtx *cli.Context) error {
 	if err != nil {
 		return err
 	}
+
+	m, err := signer.SignUpdate()
+	if err != nil {
+		return err
+	}
+
+	log.Println("-- Configure DoH client --")
+	respMsg, err := sig0.SendDOHQuery(dohServer.Host, m)
+	if err != nil {
+		return err
+	}
+
+	log.Println("-- Response from DNS server --")
+	fmt.Println(respMsg)
+
+	return nil
+}
+
+func updateRRAction(cCtx *cli.Context) error {
+	host := cCtx.Args().Get(0)
+	if host == "" {
+		return cli.Exit("host required", 1)
+	}
+	if !strings.HasSuffix(host, ".") {
+		host = host + "."
+	}
+	keystore := cCtx.String("keystore")
+
+	keys, err := sig0.ListKeysFiltered(keystore, host)
+	if err != nil {
+		return err
+	}
+	if len(keys) == 0 {
+		return cli.Exit("no key found for host", 1)
+	}
+
+	log.Println("-- Using key:", keys[0].Name)
+	// ugh.. what? doubley .key.key
+	keyPath := filepath.Join(keystore, keys[0].Name)
+	keyPath = strings.TrimSuffix(keyPath, ".key")
+	signer, err := sig0.LoadKeyFile(keyPath)
+	if err != nil {
+		return err
+	}
+
+	soa, err := sig0.QuerySOA(host)
+	if err != nil {
+		return err
+	}
+	reply, err := sig0.SendDOHQuery(sig0.DefaultDOHResolver, soa)
+	if err != nil {
+		return err
+	}
+	soaReply, err := sig0.AnySOA(reply)
+	if err != nil {
+		return err
+	}
+	zone := soaReply.Hdr.Name
+	log.Println("-- SOA lookup for", host, "found zone:", zone)
+
+	dohServer, err := sig0.FindDOHEndpoint(zone)
+	if err != nil {
+		return err
+	}
+
+	err = signer.StartUpdate(zone)
+	if err != nil {
+		return err
+	}
+
+	// read RRs from stdin
+	count := 0
+	log.Println("-- Reading RRs from stdin --")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		rrStr := scanner.Text()
+		if rrStr == "" {
+			log.Println("-- Empty line, stopping --")
+			break
+		}
+		rr, err := dns.NewRR(rrStr)
+		if err != nil {
+			return err
+		}
+		if cCtx.Bool("remove") {
+			log.Println("-- Removing RR --")
+			err = signer.RemoveRR(rr)
+		} else {
+			log.Println("-- Updating RR --")
+			err = signer.UpdateRR(rr)
+		}
+		log.Printf("-- %+v", rr)
+		count++
+		if err != nil {
+			return err
+		}
+	}
+
+	if count == 0 {
+		return cli.Exit("no RRs to update", 0)
+	}
+	log.Printf("-- Updated %d RRs --", count)
 
 	m, err := signer.SignUpdate()
 	if err != nil {
